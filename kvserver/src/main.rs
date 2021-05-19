85698dd0c3dfb77803873e2c62b30f3aa0a92079
Robert Narvaez <narvaez.rm@gmail.com>
2021-05-20 01:47:28 +0200

    add request processing

--- kvserver/src/main.rs
+++ kvserver/src/main.rs
@@ -1,11 +1,12 @@
+use std::collections::HashMap;
 use std::io::Read;
 use std::net::TcpListener;
 use std::str;
 
 #[derive(Debug)]
-enum Request<'a> {
-    Get(&'a str),
-    Set(&'a str, &'a str),
+enum Request {
+    Get(String),
+    Set(String, String),
 }
 
 fn parse_payload(payload: &str) -> Result<Request, &'static str> {
@@ -18,23 +19,35 @@ fn parse_payload(payload: &str) -> Result<Request, &'static str> {
 
     let key = match parsed.next() {
         Some(s) => s,
-        None => return Err("No key set.")
+        None => return Err("No key set."),
     };
 
     op.and_then(|o| match o {
-        "GET" => Ok(Request::Get(key)),
-        "SET" => {
-            match parsed.next() {
-                Some(s) => Ok(Request::Set(key, s)),
-                None => Err("No value set."),
-            }
-        }
-        _ => Err("Error parsing payload.")
+        "GET" => Ok(Request::Get(String::from(key))),
+        "SET" => match parsed.next() {
+            Some(s) => Ok(Request::Set(String::from(key), String::from(s))),
+            None => Err("No value set."),
+        },
+        _ => Err("Error parsing payload."),
     })
+}
+
+fn process_request(request: Request, kv: &mut HashMap<String, String>) -> Result<String, &str> {
+    match request {
+        Request::Get(s) => match kv.get(&s) {
+            Some(v) => Ok(v.to_string()),
+            None => Err("Value not found."),
+        },
+        Request::Set(k, v) => {
+            kv.entry(k).and_modify(|val| *val = v.clone()).or_insert(v);
+            Ok(String::from("Value updated."))
+        }
+    }
 }
 
 fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:8080")?;
+    let mut kv = HashMap::<String, String>::new();
 
     for stream in listener.incoming() {
         match stream {
@@ -45,10 +58,11 @@ fn main() -> std::io::Result<()> {
                 let payload = str::from_utf8(&buffer[..s_size]);
                 let request = match payload {
                     Ok(p) => parse_payload(p),
-                    Err(_) => Err("Payload error.")
+                    Err(_) => Err("Payload error."),
                 };
+                let response = request.and_then(|req| process_request(req, &mut kv));
 
-                println!("{:?}", request)
+                println!("{:?}", response)
             }
         }
     }
