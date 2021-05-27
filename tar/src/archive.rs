7b036f9b93edbb31b0721f0a1dfe4bd95e2939a1
Robert Narvaez <narvaez.rm@gmail.com>
2021-05-28 01:01:09 +0200

    initialize project

---
+++ tar/src/archive.rs
@@ -0,0 +1,8 @@
+// Archive files:
+// Get directories/files to archive
+// In a parallel manner, create file objects:
+// - Read a file
+// - Create a header based the file info
+// - Attach header and file
+// Join the file objects
+// Write the joined object into a file
---
+++ tar/src/extract.rs
@@ -0,0 +1,6 @@
+// Extract archive:
+// Read the archive file
+// Split the archive into its constituent file objects
+// In a parallel manner:
+// - Decode the header
+// - Write the file into the file system taking into account the header data
