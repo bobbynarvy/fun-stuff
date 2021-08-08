2a6a7e7198395eaebc47fbe654e96fb6665a29ce
Robert Narvaez <narvaez.rm@gmail.com>
2021-08-09 01:44:44 +0200

    add decoder

--- huffman/src/coder.nim
+++ huffman/src/coder.nim
@@ -7,6 +7,7 @@ type
     count: int
     symbol: Option[char]
     left, right: Option[Node]
+  Encoding = (char, string)
 
 func initNodes(text: string): seq[Node] =
   var char_counts = initTable[char, int]()
@@ -74,9 +75,78 @@ func encode*(text: string): string =
 
   code
 
+func insertNode(node: Node,c: char, bitstring: string) =
+  if bitstring.len == 1:
+    if bitstring == "0":
+      node.left = some(Node(count: 0, symbol: some(c), left: none(Node), right: none(Node)))
+    else:
+      node.right = some(Node(count: 0, symbol: some(c), left: none(Node), right: none(Node)))
+  else:
+    if bitstring[0] == '0':
+      if node.left.isNone:
+        node.left = some(Node(count: 0, symbol: none(char), left: none(Node), right: none(Node)))
+      insertNode(node.left.get, c, bitstring.substr(1))
+    else:
+      if node.right.isNone:
+        node.right = some(Node(count: 0, symbol: none(char), left: none(Node), right: none(Node)))
+      insertNode(node.right.get, c, bitstring.substr(1))
+
+func createTree(encodings: seq[Encoding]): Node =
+  var encodings = encodings
+
+  # The root node will always be empty.
+  # Count doesn't matter for decoding.
+  var node = Node(count: 0, symbol: none(char), left: none(Node), right: none(Node))
+
+  # sort by string length to guarantee traversal
+  # from root to lowest level
+  encodings.sort(proc (a, b: Encoding): int = cmp(a[1].len, b[1].len))
+
+  # insert nodes from root node
+  for encoding in encodings:
+    insertNode(node, encoding[0], encoding[1])
+
+  node
+
+# recursively look for the char in the encoded string;
+# return the char and the remaining encoded string
+func findChar(node: Node, code: string): (char, string) =
+  if node.left == none(Node) and node.right == none(Node):
+    return (node.symbol.get, code)
+  else:
+    let bit = code[0] 
+    let nextNode = if bit == '0': node.left.get else: node.right.get
+    return findChar(nextNode, code.substr(1))
+
+func decode*(code: string, encodings: seq[Encoding]): string =
+  let root = createTree(encodings)
+  var message = ""
+  var remainingCode = code  
+
+  while remainingCode.len != 0:
+    let result = findChar(root, remainingCode)
+    remainingCode = result[1]
+    message.add(result[0])
+
+  message
+
 when isMainModule:
   let text = "Hello, world!"
   var nodes = initNodes(text)
   var tree = createTree(nodes)
 
   doAssert tree.count == 13
+
+  var encodings: seq[Encoding] = @[('B', "10"), ('C', "11"), ('A', "0")]
+  let root = createTree(encodings)
+  let left = root.left.get[]
+  let right = root.right.get[]
+
+  doAssert left.symbol == some('A')
+  doAssert right.left.get[].symbol == some('B')
+  doAssert right.right.get[].symbol == some('C')
+
+  doAssert findChar(root, "0") == ('A', "")
+  doAssert findChar(root, "110") == ('C', "0")
+
+  doAssert decode("11010011", encodings) == "CABAC"
