ce460076006cec996b007b7a9f017ecd35326213
Robert Narvaez <narvaez.rm@gmail.com>
2021-10-30 01:10:24 +0200

    playing some more

--- basic_ocaml_proj/bin/main.ml
+++ basic_ocaml_proj/bin/main.ml
@@ -1,4 +1,12 @@
-let main () = Lwt_io.write_line Lwt_io.stdout "hello world"
+open Lwt.Infix
 
+(* Playing with Lwt to say hello world *)
 let () =
-  Lwt_main.run (main ())
+  Lwt_main.run
+    (let p1, r1 = Lwt.wait () in
+     let p2, r2 = Lwt.wait () in
+     Lwt.wakeup_later r1 "Hello" ;
+     Lwt.wakeup_later r2 "world!" ;
+     Lwt.all [p1; p2]
+     >|= (fun p -> String.concat " " p)
+     >>= fun x -> Lwt_io.printl x )
