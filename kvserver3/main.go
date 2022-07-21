079bcb6ce7faa1bed00a8a69dc70fbbbadf8cd11
Robert Narvaez <narvaez.rm@gmail.com>
2022-07-21 22:57:29 +0000

    add server

--- kvserver3/main.go
+++ kvserver3/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"errors"
 	"fmt"
+	"log"
+	"net"
 	"strings"
 )
 
@@ -26,11 +28,11 @@ func parsePayload(payload string) (*Request, error) {
 			return nil, errors.New("Missing GET argument.")
 		}
 		return &Request{"GET", parsed[1:2]}, nil
-	case "POST":
+	case "SET":
 		if len(parsed) < 3 {
-			return nil, errors.New("Missing POST arguments.")
+			return nil, errors.New("Missing SET arguments.")
 		}
-		return &Request{"POST", parsed[1:3]}, nil
+		return &Request{"SET", parsed[1:3]}, nil
 	default:
 		return nil, errors.New("Invalid method.")
 	}
@@ -42,27 +44,65 @@ func processRequest(request *Request, kv KVStore) (string, error) {
 	switch request.method {
 	case "GET":
 		if val, ok := kv[key]; ok {
+			log.Printf("GET %s => %s", key, val)
 			return val, nil
 		}
 
 		return "", errors.New("Value not found.")
-	case "POST":
-		value := request.value[1]
-		kv[key] = value
+	case "SET":
+		val := request.value[1]
+		kv[key] = val
+		log.Printf("SET %s to %s", key, val)
 
-		return fmt.Sprintf("Updated value of %s with %s", key, value), nil
+		return fmt.Sprintf("Updated value of %s with %s", key, val), nil
 	default:
 		return "", errors.New("Invalid method.")
 	}
 }
 
+func handleConn(conn net.Conn, kv KVStore) {
+	buffer := make([]byte, 1024)
+	_, err := conn.Read(buffer)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	payload := string(buffer)
+	log.Printf("Received payload: %s", payload)
+	parsed, err := parsePayload(payload)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	result, err := processRequest(parsed, kv)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	conn.Write([]byte(result))
+	conn.Close()
+}
+
 func main() {
-	m := make(KVStore)
-	payload := "POST hello world"
-	parsed, _ := parsePayload(payload)
-	processRequest(parsed, m)
-	payload = "GET hello"
-	parsed, _ = parsePayload(payload)
-	result, _ := processRequest(parsed, m)
-	fmt.Println(result)
+
+	kv := make(KVStore)
+
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer listener.Close()
+
+	log.Println("Key-value server listening...")
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		handleConn(conn, kv)
+	}
 }
