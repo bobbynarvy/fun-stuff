c6e4294ae311d86fb50da5dd8d35cde3884ce3f7
Robert Narvaez <narvaez.rm@gmail.com>
2022-07-27 23:29:23 +0000

    add reader

---
+++ huffman-go/io/reader.go
@@ -0,0 +1,50 @@
+package io
+
+import (
+	"bufio"
+	"fmt"
+	"huffman/coder"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func DecodeFromFile(fileName string) (string, error) {
+	if fileName == "" {
+		fileName = "encoded.huff"
+	}
+
+	file, err := os.Open(fileName)
+	if err != nil {
+		return "", err
+	}
+
+	//  read first line to get the number of encoding pairs
+	scanner := bufio.NewScanner(file)
+	scanner.Scan()
+	numPairs, _ := strconv.Atoi(scanner.Text())
+
+	// get the encoding pairs
+	pairs := []coder.EncodingPair{}
+	for i := 1; i <= numPairs; i++ {
+		scanner.Scan()
+		pairString := scanner.Text()
+		charCode := strings.Split(pairString, " ")
+		char, _ := strconv.Atoi(charCode[0])
+		encodingPair := coder.EncodingPair{
+			Char: rune(char),
+			Code: charCode[1],
+		}
+		pairs = append(pairs, encodingPair)
+	}
+
+	// get the encoded text
+	scanner.Scan()
+	codedText := scanner.Text()
+
+	decoded := coder.Decode(codedText, pairs)
+
+	fmt.Printf("Decoded text: %s\n", decoded)
+
+	return decoded, nil
+}
