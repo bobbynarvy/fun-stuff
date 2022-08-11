40ff7c18809ade8c1365846cc38f7f78aafa0dcc
Robert Narvaez <narvaez.rm@gmail.com>
2022-08-11 16:09:21 +0000

    add more queries

--- quotes/db/query.sql
+++ quotes/db/query.sql
@@ -1,3 +1,24 @@
 -- name: ListAuthors :many
 SELECT * FROM authors
 ORDER BY name;
+
+-- name: GetAuthor :one
+SELECT * FROM authors
+WHERE id = $1;
+
+-- name: ListQuotes :many
+SELECT q.id, q.quote, a.name AS author
+FROM quotes q
+INNER JOIN authors a ON q.author_id = a.id;
+
+-- name: GetQuote :one
+SELECT q.id, q.quote, a.name AS author
+FROM quotes q
+INNER JOIN authors a ON q.author_id = a.id
+WHERE q.id = $1;
+
+-- name: GetQuotesByAuthor :many
+SELECT q.id, q.quote, a.name AS author
+FROM quotes q
+INNER JOIN authors a ON q.author_id = a.id
+WHERE a.id = $1;
