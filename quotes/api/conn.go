0e453e7fca9888f4212b51374782f674b1dfb038
Robert Narvaez <narvaez.rm@gmail.com>
2022-08-13 14:48:06 +0000

    move connection to own file

---
+++ quotes/api/conn.go
@@ -0,0 +1,31 @@
+package api
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"os"
+)
+
+func getEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+
+	return fallback
+}
+
+func connect() (*sql.DB, error) {
+	dbHost := getEnv("MYSQL_HOST", "localhost")
+	dbPort := getEnv("MYSQL_PORT", "3306")
+	dbUser := getEnv("MYSQL_USER", "root")
+	dbPass := getEnv("MYSQL_PASSWORD", "password")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/quotes", dbUser, dbPass, dbHost, dbPort)
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
--- quotes/api/server.go
+++ quotes/api/server.go
@@ -2,42 +2,15 @@ package api
 
 import (
 	"context"
-	"database/sql"
 	"encoding/json"
 	"fmt"
-	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
-	"os"
 	"quotes/data"
 )
 
-func getEnv(key, fallback string) string {
-	if value, ok := os.LookupEnv(key); ok {
-		return value
-	}
-
-	return fallback
-}
-
-func connect() (*sql.DB, error) {
-	dbHost := getEnv("MYSQL_HOST", "localhost")
-	dbPort := getEnv("MYSQL_PORT", "3306")
-	dbUser := getEnv("MYSQL_USER", "root")
-	dbPass := getEnv("MYSQL_PASSWORD", "password")
-
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/quotes", dbUser, dbPass, dbHost, dbPort)
-	db, err := sql.Open("mysql", dsn)
-	if err != nil {
-		return nil, err
-	}
-
-	return db, nil
-}
-
 func authorsHandler(w http.ResponseWriter, req *http.Request) {
-	ctx := context.Background()
 	db, err := connect()
 	if err != nil {
 		log.Fatal(err)
@@ -46,6 +19,7 @@ func authorsHandler(w http.ResponseWriter, req *http.Request) {
 
 	queries := data.New(db)
 
+	ctx := context.Background()
 	authors, err := queries.ListAuthors(ctx)
 	if err != nil {
 		log.Fatal(err)
